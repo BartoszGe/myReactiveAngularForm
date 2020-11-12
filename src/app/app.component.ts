1048e2ecc1036d35e694bab9762bff0f39c88f46
bgertych <bartosz.gertych@tivo.com>
2020-11-12 09:14:21 +0100

    add angular reactive template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'playground-forms-reactive-app';
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+  forbiddenUsernames: string[];
+
+  ngOnInit(): void {
+    this.forbiddenUsernames = ['Kuba', 'Anna'];
+    this.signupForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null, [Validators.required, this.forbiddenNameValidator.bind(this)]),
+        email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmailValidator)
+      }),
+      gender: new FormControl('male'),
+      hobbies: new FormArray([])
+    });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value => console.log(value))
+    // );
+    // this.signupForm.statusChanges.subscribe(
+    //   (status => console.log(status))
+    // );
+  }
+
+  onSubmit(): void {
+    console.log(this.signupForm);
+  }
+
+  onAddHobby(): void {
+    const control = new FormControl(null, Validators.required);
+    (this.signupForm.get('hobbies') as FormArray).push(control);
+  }
+
+  getControls(): AbstractControl[] {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+
+  forbiddenNameValidator(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.includes(control.value)) {
+      return {nameIsForbidden: true};
+    }
+    return null;
+  }
+
+  forbiddenEmailValidator(control: FormControl): Promise<any> | Observable<any> {
+
+    return new Promise<any>(((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({emailIsForbidden: true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    }));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
